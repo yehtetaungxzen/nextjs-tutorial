d1875871fdac8ca67c5b89cbaebb1fd6c665fd06
yellhtetaung <yehtetaung.dev@gmail.com>
2023-11-02 11:51:41 +0630

    init

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,13 +27,13 @@ const MyProfile = () => {
 	};
 
 	const handleDelete = async (post) => {
-		const hashConfirmed = confirm("Are you sure you want to delete");
+		const hasConfirmed = confirm(
+			"Are you sure you want to delete this prompt?"
+		);
 
-		if (hashConfirmed) {
+		if (hasConfirmed) {
 			try {
-				await fetch(`api/prompt/${post._id.toString()}`, {
-					method: "DELETE",
-				});
+				await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
 
 				const filteredPosts = posts.filter((p) => p._id !== post._id);
 
--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,14 +1,12 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@/components/Form";
 
 const EditPrompt = () => {
 	const router = useRouter();
-	const { data: session } = useSession();
 	const searchParams = useSearchParams();
 	const promptId = searchParams.get("id");
 
